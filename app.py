8f2f71fdd9ca4372efe378ac6428aecc25a34be3
Genicleito Gonçalves <5203963+Genicleito@users.noreply.github.com>
2025-02-14 15:41:49 -0300

    fix

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ if aportes and periodo_anos and taxa_juros_ano:
 
     # st.markdown(f"\n> Gráfico de pizza")
     fig = px.pie(
-        pd.DataFrame({'x': [total_em_aportes], 'y': [total_em_juros]}),
+        pd.DataFrame({'Valor': [total_em_aportes, total_em_juros], 'Distribuição': ['Valor investido', 'Total em juros']}),
         values='x',
         names='y',
         title='Distribuição dos valores aportados e rendimentos'

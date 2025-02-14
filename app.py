ccb8eac1e49a294941b19c8c8ad96d60478a0f2b
Genicleito Gonçalves <5203963+Genicleito@users.noreply.github.com>
2025-02-14 16:00:26 -0300

    refact

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ if aportes and periodo_anos and taxa_juros_ano:
 
     st.dataframe(
         # df.round(2).style.format({'Valor investido': 'R$ {:.2f}', 'Valor resultado (com os juros)': 'R$ {:.2f}'}).sort_values("Mês"),
-        df.round(2).sort_values("Mês").format({
+        df.round(2).sort_values("Mês").style.format({
             "Valor investido": lambda x: f"${x:,.2f}".replace('.', '').replace(',', '.'),
             "Valor resultado (com os juros)": lambda x: f"${x:,.2f}".replace('.', '').replace(',', '.'),
         }),

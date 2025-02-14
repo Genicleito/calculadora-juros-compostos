5f2f71ae344c18c9ed30439e87c47ee86c53a803
Genicleito Gonçalves <5203963+Genicleito@users.noreply.github.com>
2025-02-14 15:57:05 -0300

    fix

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ if aportes and periodo_anos and taxa_juros_ano:
 
     st.dataframe(
         # df.round(2).style.format({'Valor investido': 'R$ {:.2f}', 'Valor resultado (com os juros)': 'R$ {:.2f}'}).sort_values("Mês"),
-        pd.DataFrame.round(2).sort_values("Mês").format({
+        df.round(2).sort_values("Mês").format({
             "Valor investido": lambda x: f"${x:,.2f}".replace('.', '').replace(',', '.'),
             "Valor resultado (com os juros)": lambda x: f"${x:,.2f}".replace('.', '').replace(',', '.'),
         }),

a0b12ca31b9204a9a977b73ddcda743d3000cfb7
Genicleito Gonçalves <5203963+Genicleito@users.noreply.github.com>
2025-02-14 16:04:18 -0300

    refact

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if aportes and periodo_anos and taxa_juros_ano:
     st.markdown(f"> Em {periodo_anos} anos você terá **R\$ {portfolio_final:.2f}**.")
     st.write(f"Deste valor, saíram do seu bolso como investimento apenas R\$ {total_em_aportes:.2f} e R\$ {total_em_juros} foi o que você teve de rendimento com os juros.")
     st.markdown(f"""
-    > As configurações utilizadas para gerar esses valores foram:
+    **As configurações utilizadas para gerar esses valores foram:**
 
     Saldo inicial: \t\tR$ {valor_inicial:.2f}
     Aplicações mensais: \t\tR$ {aportes:.2f}
